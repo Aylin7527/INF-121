d0a3ef67aa860f227c48fc2fc188e13955b30abb
Aylin7527 <149416390+Aylin7527@users.noreply.github.com>
2025-02-18 18:23:01 -0400

    Add files via upload

---
+++ PracticaNro2/Java/Punto/src/main/java/Linea.java
@@ -0,0 +1,17 @@
+public class Linea {
+    public Punto p1, p2;
+
+    public Linea(Punto p1, Punto p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double longitud() {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Linea: %s -> %s, Longitud: %.2f", p1, p2, longitud());
+    }
+}
